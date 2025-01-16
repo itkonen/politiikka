13ac0eb9e5897b4802d6c6bc1d51a147b33066c0
Juha Itkonen <itkonen.juha@gmail.com>
2025-01-16 19:00:28 +0200

    Upgrade theme

--- dev.R
+++ dev.R
@@ -2,9 +2,10 @@ library(blogdown)
 
 ## new_site(theme = "devcows/hugo-universal-theme")
 
-serve_site(host = "0.0.0.0", port = "8081")
+serve_site(host = "0.0.0.0", port = "8082")
 
 stop_server()
 
 build_site()
 
+blogdown::install_theme(theme = "devcows/hugo-universal-theme", force=TRUE, update_config=F)
